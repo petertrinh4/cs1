54c7bddd8e9d7b14429cc9c97ec4fb3c83b8252f
Peter Trinh <petertrinh04@gmail.com>
2025-05-30 23:27:55 -0400

    Programming Assignment 1

--- pa1.c
+++ pa1.c
@@ -1,61 +1,69 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-int searchID(int ID[], int targetID, int arrayLength);
+typedef struct { //STUDENT STRUCTURE
+    int ID;
+    char firstName[20];
+    char lastName[20];
+    float GPA;
+}Student;
+
+int searchStudentID(Student student_Arr[], int lengthArray, int searchValue);
 
 int main() {
     char fileName[30];
-    FILE *input;
-    int targetID;
+    FILE *file; //DECLARING VARIABLES
+    Student student_Arr[100];
+    int index = 0;
+    int searchOrQuit;
+    int searchResult;
 
-    int ID[100];
-    char **fNarr = (char **)malloc(100 * sizeof(char *));
-    char **lNarr = (char **)malloc(100 * sizeof(char *));
-    float GPA[100];
+    printf("Enter the file name to process: "); //ASK USER TO ENTER FILE NAME
+    scanf("%s", fileName);
+    file = fopen(fileName, "r"); //OPENS FILE
 
-    input = fopen("students.txt", "r");
-
-    printf("Enter the file name to process: "); //Asks user for file name to process
-    scanf("%s", fileName); //Scans file name from user
-
-    while (fscanf(input, "%d %s %s %.1lf", &ID, fNarr, lNarr, &GPA) != EOF) { //Reads file then assigns the values to respective variables
-        for (int index = 0; index < ID; index++) {
-            if (ID[index] > ID[index + 1]) {
-                printf("Invalid File, ID's aren't sorted in increasing order.");
-                break;
-            }
-            else {
-                printf("Enter a student ID to search or -1 to quit: ");
-                scanf("%d", targetID);
-
-                if (targetID != -1) {
-                int searchResult = searchID(ID, targetID, 100); //FIXME: WRONG LENGTH?
-                    if (searchResult == 1) { 
-                        printf("[%d, %s, %s, %.1lf]", ID, fNarr, lNarr, GPA); //FIXME: WRONG VARIABLES
-                    }
-                    else {
-                        printf("This ID is not in the records.");
-                    }
-                }
-                else break;
-            }
+    while(fscanf(file, "%d %s %s %f",
+                &student_Arr[index].ID, //SCANS THROUGH FILE & ASSIGNS DATA MEMBERS TO STRUCTURE ARRAY
+                 student_Arr[index].firstName,
+                 student_Arr[index].lastName,
+                &student_Arr[index].GPA) == 4) {
+    
+        if(index > 0 && student_Arr[index].ID <= student_Arr[index - 1].ID) { //CHECKS FOR INCREASING ID ORDER IN DATA
+            printf("Invalid file, student record ID's must be in increasing order.");
+            fclose(file);
+            return 1;
         }
+        index++;
+        if(index >= 100) break; //COUNTS NUMBER OF STUDENTS
     }
+        while(1) { //SEARCHING STUDENT
+            printf("Enter a student ID to search or -1 to quit: ");
+            scanf("%d", &searchOrQuit);
+
+            if(searchOrQuit == -1) break;
+            searchResult = searchStudentID(student_Arr, index, searchOrQuit);
+            if(searchResult != -1) {
+                printf("%d, %s %s, GPA: %.1f\n", student_Arr[searchResult].ID, 
+                                                   student_Arr[searchResult].firstName, 
+                                                   student_Arr[searchResult].lastName, 
+                                                   student_Arr[searchResult].GPA);
+            }
+            else {printf("This ID is not in the records.\n");}
+        }
     return 0;
 }
 
-int searchID(int ID[], int targetID, int arrayLength) {
-    int lowIndex = 0, highIndex = arrayLength - 1;
+int searchStudentID(Student student_Arr[], int lengthArray, int searchValue) {
+    int low = 0, high = lengthArray - 1;
 
-    while(lowIndex <= highIndex) {
-        int middleIndex = lowIndex + (highIndex - lowIndex) / 2;
-        
-        if(targetID == ID[middleIndex]) {return targetID;} //FIXME: RETURN WHAT?
+    while(low <= high) {
+        int middle = low + (high - low) / 2;
 
-        else if(targetID > ID[middleIndex]) {lowIndex = middleIndex + 1;}
-        
-        else {highIndex = middleIndex - 1;}
+        if(searchValue == student_Arr[middle].ID) {return middle;}
+        else if(searchValue < student_Arr[middle].ID) {high = middle - 1;}
+        else {low = middle + 1;}
     }
-
     return -1;
 }
